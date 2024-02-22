74eecb1b36054ac6f6d0d816b80b324b7b92bdd7
Simran Yadav <simranyadav464@gmail.com>
2024-02-22 23:22:08 +0530

    Updated the code

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const scrapeEventDetails = require('./src/scraper/eventbriteScaper');
+const scrapeEventDetails = require('./src/scraper/eventbriteScraper');
 const enrichDataWithGoogleSearch = require('./src/scraper/googleSearchScraper');
 
 // Example usage
 const eventbriteUrl = 'https://www.eventbrite.com/';
-scrapeEventDetails(eventbriteUrl)
-  .then(async (events) => {
+
+async function run() {
+  try {
+    // Scrape event details
+    const events = await scrapeEventDetails(eventbriteUrl);
     console.log('Event Details:', events);
 
     // Enrich data via Google Search
     await enrichDataWithGoogleSearch(events);
 
+    // Log enriched event details
     console.log('Enriched Event Details:', events);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error('Error:', error.message);
-  });
+  }
+}
+
+// Run the script
+run();
